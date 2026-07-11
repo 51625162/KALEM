b4dea1887b470b71237ef769def30a9bab50fde1
51625162 <kasim.merkez.02@gmail.com>
2026-07-11 12:05:01 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# KALEM-V1 Paket 1
+
+İçerik:
+- Dashboard
+- Sol menü
+- Bildirim testi
+- LocalStorage altyapısı
+- HTML/CSS/JS başlangıç yapısı
---
+++ script.js
@@ -0,0 +1,4 @@
+function showNotification(){
+document.getElementById('notification').textContent='Paket 1 başarıyla çalışıyor.';
+localStorage.setItem('kalem_status','Paket1');
+}
